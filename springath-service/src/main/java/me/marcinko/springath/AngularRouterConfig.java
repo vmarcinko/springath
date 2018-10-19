b5b93de3071f45e1c227866c7a01e009d38e8f36
Vjeran Marcinko <vmarcinko@gmail.com>
2018-10-19 08:30:14 +0200

    Angular Spring router URLs config

---
+++ springath-service/src/main/java/me/marcinko/springath/AngularRouterConfig.java
@@ -0,0 +1,18 @@
+package me.marcinko.springath;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * If one refresh the browser on some web URL, or enters such URL directly, we need to forward the request to index.html for Angular app to process the URl before
+ * web servers returns 404 for non-existent resource.
+ */
+@Configuration
+public class AngularRouterConfig implements WebMvcConfigurer {
+	@Override
+	public void addViewControllers(final ViewControllerRegistry registry) {
+		registry.addViewController("/heroes").setViewName("forward:/");
+		registry.addViewController("/dashboard").setViewName("forward:/");
+	}
+}
